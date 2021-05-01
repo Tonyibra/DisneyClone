161a7bff87dbbf1c435b66cef678dbc68001a334
Tony ibrahim <tonyibra.ads@gmail.com>
2021-05-01 23:18:52 +0300

    ADD: viewers

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Viewers from "./Viewers";
 const Home = () => {
 	return (
 		<Container>
 			<ImageSlider />
+			<Viewers />
 		</Container>
 	);
 };
@@ -15,7 +17,7 @@ const Container = styled.main`
 	min-height: calc(100vh - 70px);
 	padding: 0 calc(3.5vw + 5px);
 	position: relative;
-
+	overflow-x: hidden;
 	&:before {
 		background: url("/img/home-background.png") center center / cover no-repeat
 			fixed;
